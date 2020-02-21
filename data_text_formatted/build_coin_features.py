db2328bb5785c542ae1166458aa55621e96f18e1
statevector <cwillis@cern.ch>
2020-02-21 00:06:01 -0700

    Extended formatting functions to handle numerous edge cases

--- data_text_formatted/build_coin_features.py
+++ data_text_formatted/build_coin_features.py
@@ -23,7 +23,7 @@ grades = ['FDC', 'Superb EF', 'Choice EF', 'Near EF', 'EF', 'Nice VF',
 # convert all dash types (hyphen, en-, em-, minus, others?) to a
 # common dash to simplify analysis later
 # https://en.wikipedia.org/wiki/Dash#Similar_Unicode_characters
-def format_dash(text):
+def format_dashes(text):
 	#text = text.encode('utf-8')
 	text = text.replace(u'\xa0', u' ')   # ???
 	text = text.replace(u'\u2010', u'-') # hyphen
@@ -36,6 +36,23 @@ def format_dash(text):
 	text = text.replace(u'\u2212', u'-') # minus sign
 	return text
 
+# identify textual inconsistencies
+def format_spaces(text, verbose=False):
+	# double space
+	result = re.search(r'  ', text)
+	if result is not None:
+		if(verbose): print('double space:\n{}'.format(text))
+		text = re.sub(r'  ', r' ', text)
+		if(verbose): print('double space:\n{}'.format(text))
+	# space followed by period
+	result = re.search(r' \.', text)
+	if result is not None:
+		if(verbose): print('space followed by period:\n{}'.format(text))
+		text = re.sub(r' \.', r'.', text)
+		if(verbose): print('space followed by period:\n{}'.format(text))
+	return text
+
+# probably want to fold this into the format_measurements function
 def clean_measurements(result):
 	# consolidate measurement variations
 	result = result.replace('  g', ' g')
@@ -44,9 +61,10 @@ def clean_measurements(result):
 	result = result.replace('  gm', ' gm')
 	result = result.replace(' gm', 'gm')
 	result = result.replace('gm', 'g')
-	# removes spaces
+	# remove initial spaces
 	result = result.replace(' mm', 'mm')
 	result = result.replace(' h', 'h')
+	# remove final spaces
 	return result
 
 # format size, weight, orientation information
@@ -68,8 +86,8 @@ def format_measurements(text):
 			result = clean_measurements(result)
 			text = re.sub(regex, result, text)
 			return text
-		# case 3: missing 'g' and 'h'
-		regex = r'\(.+mm.+\)'
+		# case 3: missing 'g' and 'h' (no end space)
+		regex = r'\(.+mm\)'
 		result = re.search(regex, text)
 		if result is not None:
 			result = result.group(0)
@@ -84,7 +102,16 @@ def format_measurements(text):
 			result = clean_measurements(result)
 			text = re.sub(regex, result, text)
 			return text
-		# case 5, 6, 7, ...
+		# case 5: missing 'g' and 'h' (with ending space)
+		# regex = r'\(.+mm \)'
+		# result = re.search(regex, text)
+		# if result is not None:
+		# 	result = result.group(0)
+		# 	result = result.replace('mm ', 'mm')
+		# 	text = re.sub(regex, result, text)
+		# 	print(text)
+		# 	return text
+		# case 6, 7, 8, ...
 		raise TypeError()
 	except:
 		exit('unable to format measurements for {}'.format(text))
@@ -92,30 +119,84 @@ def format_measurements(text):
 # consolidate similar grades;
 # e.g. 'Near EF', 'Good VF', 'Nice VF', etc.
 def format_grade(text):
-	pass
-
-
-
-def format_mint(text):
-	text = re.sub(r'Rome mint;', 
-		'Rome mint.', text)
-	text = re.sub(r'Emerita mint;', 
-		'Emerita (Mérida) mint.', text)
-	text = re.sub(r'Uncertain Spanish mint \(Colonia Patricia\?\)', 
-		'Spanish mint (Colonia Patricia?)', text)
-	text = re.sub(r'Colonia Patricia\(\?\) mint', 
-		'Spanish mint (Colonia Patricia?)', text)
-	text = re.sub(r'Uncertain Spanish mint \(Colonia Caesaraugusta\?\)', 
-		'Spanish mint (Colonia Caesaraugusta?)', text)
 	# ...
 	return text
 
+def format_mint(text):
+	# Rome
+	text = re.sub(r'Rome mint;', 'Rome mint.', text)
+	#text = re.sub(r' Rome mint\.', 'Rome mint.', text) # Augustus EA4
+	# Emerita
+	text = re.sub(r'Emerita mint\.', 'Emerita (Mérida) mint.', text)
+	text = re.sub(r'Emerita mint;', 'Emerita (Mérida) mint;', text)
+	text = re.sub(r'Spanish mint - Emerita', 'Emerita (Mérida) mint', text) # Augustus EA4
+	# Colonia Patricia
+	text = re.sub(r'Uncertain Spanish mint \(Colonia Patricia\?\)', 'Spanish mint (Colonia Patricia?)', text)
+	text = re.sub(r'Colonia Patricia\(\?\) mint', 'Spanish mint (Colonia Patricia?)', text)
+	text = re.sub(r'Colonia Patricia mint', 'Spanish mint (Colonia Patricia)', text) # Augustus EA4
+	# Colonia Caesaraugusta
+	text = re.sub(r'Uncertain Spanish mint \(Colonia Caesaraugusta\?\)', 'Spanish mint (Colonia Caesaraugusta?)', text)
+	text = re.sub(r'Spanish mint \(Caesaraugusta\?\)', 'Spanish mint (Colonia Caesaraugusta?)', text) # Augustus EA4
+	text = re.sub(r'Caesaraugusta mint', 'Spanish mint (Colonia Caesaraugusta)', text) # Augustus EA4
+	text = re.sub(r'Spanish mint \(Colonia Caesaraugusta\?\)', 'Spanish mint (Colonia Caesaraugusta?)', text) # Augustus EA4
+	text = re.sub(r'Spanish mint \(Caesaraugusta\?\)', 'Spanish mint (Colonia Caesaraugusta?)', text) # Augustus EA4
+	# Tarraco
+	text = re.sub(r'Spanish mint - Tarraco', 'Tarraco mint', text) # Augustus EA4
+	# Lugdunum (Lyon) mint
+	text = re.sub(r'Lugdunum \(Lyons\) mint', 'Lugdunum (Lyon) mint', text) # Augustus EA4
+	# Spain
+	text = re.sub(r'Uncertain Spanish mint', 'Spanish mint', text) # Augustus EA4
+	# ...
+	return text
+
+def format_abbreviations(text):
+	text = re.sub(r'var\.', 'variation', text)
+	text = re.sub(r'cf\.', 'confer', text)
+	text = re.sub(r'Cf\.', 'Confer', text)
+	text = re.sub(r'p\. ', 'page ', text) # space is crucial to avoid observations that end 'edge chip.'
+	text = re.sub(r'rev\. ', 'reverse ', text)
+	text = re.sub(r'obv\. ', 'obverse ', text)
+	text = re.sub(r'corr\. ', 'correction ', text)
+	return text
+
+# check for existence of 'Stuck' keyword and append
+# after the 'mint' field if not present
+def impute_strike(text, verbose=False):
+	result = re.search(r'Struck ', text)
+	if result is None:
+		if(verbose): print('before strike: {}'.format(text))
+		text = re.sub(r'mint.', 'mint. Struck unlisted.', text)
+		if(verbose): print('after strike: {}'.format(text))
+	return text
+
+# check for existence of 'mint' keyword and append
+# before the 'Stuck' field if not present
+def impute_mint(text, verbose=False):
+	result = re.search(r'mint', text)
+	if result is None:
+		if(verbose): print('before mint: {}'.format(text))
+		idx = -1
+		segments = text.split('.')
+		for index, segment in enumerate(segments):
+			if 'Struck' in segment:
+				# get the index following where
+				# 'Struck' was found
+				idx = index + 1
+		# remember the space
+		segments.insert(idx, ' Unlisted mint')
+		text = '.'.join(segments)
+		if(verbose): print('after mint: {}'.format(text))
+	return text
 
 def format_description(text):
-	text = format_dash(text)
+	text = format_dashes(text)
+	text = format_spaces(text)
 	text = format_measurements(text)
 	#text = format_grade(text)
 	text = format_mint(text)
+	text = format_abbreviations(text)
+	text = impute_strike(text)
+	text = impute_mint(text)
 	return text
 
 def list_csv_files(path):
@@ -159,9 +240,10 @@ def get_measurements(text):
 			#print(result)
 			size, weight = result.split(' ')
 			hour = None
+			#print("warning hour is none")
 			return pd.Series([size, weight, hour])
-		# case 3: missing 'g' and 'h'
-		result = re.search(r'\(.+mm.+\)', text)
+		# case 3: missing 'g' and 'h' (no space at end)
+		result = re.search(r'\(.+mm\)', text)
 		if result is not None:
 			result = result.group(0)
 			result = result[1:-1] # remove '(' and ')'
@@ -179,6 +261,18 @@ def get_measurements(text):
 			size = None
 			weight, hour = result.split(' ')
 			return pd.Series([size, weight, hour])
+		# case 5: missing 'g' and 'h' (with space at end)
+		# result = re.search(r'\(.+mm \)', text)
+		# if result is not None:
+		# 	result = result.group(0)
+		# 	result = result[1:-1] # remove '(' and ')'
+		# 	print(result)
+		# 	size = result.split(' ')
+		# 	print(size)
+		# 	quit()
+		# 	weight = None
+		# 	hour = None
+		# 	return pd.Series([size, weight, hour])
 		raise TypeError()
 	except:
 		exit('unable to extract measurements for {}'.format(text))
@@ -218,20 +312,19 @@ def get_RIC_number(text):
 		return result.group(0)
 	return None
 
-# crudely assume the longest segment contains
-# the coin imagery for this first iteration
-def get_imagery(text):
-	# isolate the description 
-	lower = text.find('mint')
+# Assume for now longest segment between 'Struck' 
+# and 'RIC' fields contains the coin imagery
+def get_imagery(text, verbose=True):
+	lower = text.find('Struck')
 	upper = text.find('RIC')
 	if lower>0 and upper>0:
 		text = text[lower:upper]
 	else:
 		return None
-	print(text)
+	# identify longest segment
 	segments = text.split('.')
 	length = 0
-	imagery = ""
+	imagery = None
 	for segment in segments:
 		if len(segment)>length:
 			imagery = segment
@@ -259,8 +352,8 @@ def stem_comments(text):
 	text = text.replace('toning', 'tone')
 	text = text.replace('attractively', 'attractive')
 	text = text.replace('iridescence', 'iridescent')
+	text = text.replace('darkly', 'dark')
 	text = text.replace('portraiture', 'portrait')
-	text = text.replace('scarse', 'rare')
 	text = text.replace('centered', 'center')
 	# but not marks --> mark or scratches --> scratch. probably info in the plural form.
 	text = text.replace('roughness', 'rough')
@@ -279,18 +372,28 @@ def stem_comments(text):
 	text = text.replace('struck', 'strike')
 	text = text.replace('banker’s', 'banker') # standardize apostrophes?
 	text = text.replace('bankers’', 'banker')
+	text = text.replace('cleaned', 'clean')
+	text = text.replace('cleaning', 'clean')
+	text = text.replace('off center', 'off-center')
+	return text
+
+# condidate descriptions where necessary
+def consolidate_descriptors(text):
+	text = text.replace('scarse', 'rare')
 	# maybe combine these?
 	#'golden' in text or 'rose' in text or 'blue' in text or 'rainbow' in text:
 	return text
 
 def get_comments(text):
 	# isolate the comments section. We know it
-	# occurs after the RIC number is presented.
-	comments = ""
+	# occurs AFTER the RIC number.
+	comments = None
+	#print('pre Isolate RIC:\n {}'.format(text))
 	lower = text.find('RIC') # add ACIP; RSC as backups
 	if lower>0:
 		comments = text[lower:]
 	else:
+		quit('Error: RIC not identified')
 		return None
 	#print('post Isolate RIC:\n {}'.format(comments))
 	# remove RIC clause
@@ -298,7 +401,7 @@ def get_comments(text):
 	comments = comments[1:] 
 	comments = ' '.join(comments)
 	#print('post Remvove RIC:\n {}'.format(comments))
-	# remove grade
+	# remove grade if present
 	for grade in grades:
 		if grade in comments:
 			comments = comments.replace(grade, '')
@@ -315,10 +418,10 @@ if __name__ == '__main__':
 
 	file = '/Users/cwillis/GitHub/RomanCoinData/data_text/Augustus_AR_EA1.csv' # 193/193
 	file = '/Users/cwillis/GitHub/RomanCoinData/data_text/Augustus_AR_EA2.csv' # 191/192, NGC encapsulation
-	file = '/Users/cwillis/GitHub/RomanCoinData/data_text/Augustus_AR_EA3.csv' # 193/195, checks out (each missing h)
-	file = '/Users/cwillis/GitHub/RomanCoinData/data_text/Augustus_AR_EA4.csv' # older, needs checks, especially comments
-	file = '/Users/cwillis/GitHub/RomanCoinData/data_text/Augustus_AR_PA1.csv' # 119/119, comments!
-
+	file = '/Users/cwillis/GitHub/RomanCoinData/data_text/Augustus_AR_EA3.csv' # 193/195, checks out (each null missing h)
+	#file = '/Users/cwillis/GitHub/RomanCoinData/data_text/Augustus_AR_EA4.csv' # checked. good.
+	#file = '/Users/cwillis/GitHub/RomanCoinData/data_text/Augustus_AR_PA1.csv' # 119/119, comments!
+	
 	df = pd.read_csv(file)
 	print(df.info())
 
@@ -344,6 +447,7 @@ if __name__ == '__main__':
 	# df['Size'], df['Weight'], df['Hour'] = df['Description'].apply(lambda x: get_measurements(x))
 	df[['Size', 'Weight', 'Hour']] = df['Description'].apply(lambda x: get_measurements(x))
 	print(df.info())
+
 
 	df['Mint'] = df['Description'].apply(lambda x: get_mint(x))
 	print(df.info())
