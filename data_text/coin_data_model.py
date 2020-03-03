49c056d131e2809c265a6bb85d16f5415bf644af
statevector <cwillis@cern.ch>
2020-03-03 01:35:37 -0700

    starting vectorizer and regression tests

--- data_text/coin_data_model.py
+++ data_text/coin_data_model.py
@@ -44,8 +44,19 @@ class TextTransformer(base.BaseEstimator, base.TransformerMixin):
 
 if __name__ == '__main__':
 
+	# min/max df tests
+	# cv = CountVectorizer(min_df=2, max_df=1.0, lowercase=True) 
+	# # here is just a simple list of 3 documents.
+	# corpus = ['one two three everywhere', 'four five six everywhere', 'seven eight nine everywhere']
+	# # below we call fit_transform on the corpus and get the feature names.
+	# X = cv.fit_transform(corpus)
+	# vocab = cv.get_feature_names()
+	# print(vocab)
+	# print(X.toarray())
+	# print(cv.stop_words_)
+
 	data = pd.read_csv('/Users/cwillis/GitHub/RomanCoinData/data_text/data_prepared/Augustus.csv')
-	print(data.shape)
+	print('data shape: {}'.format(data.shape))
 
 	#data['d2'] = data['d2'].apply(lambda x: str(x).replace('λ', 'a'))
 	#data['d2'] = data['d2'].apply(lambda x: str(x).replace('avg', 'aug'))
@@ -53,7 +64,7 @@ if __name__ == '__main__':
 	#data['d2'] = data['d2'].apply(lambda x: str(x).split())
 
 	words = count_unique_words(data['Comments'])
-	print(words)
+	#print(words)
 
 	# text formatting on the input data set
 	#ttransformer = TextTransformer()
@@ -73,31 +84,34 @@ if __name__ == '__main__':
 	#stemmer = SnowballStemmer('english')
 	#data['d3'] = data['d2'].apply(lambda x: ' '.join([stemmer.stem(word) for word in x.split()]))
 
-	#vectorizer = CountVectorizer() # (3867, 4528)
-	#vectorizer = CountVectorizer(stop_words='english') # (3867, 4363)
-	#vectorizer = CountVectorizer(min_df=30) # (3867, 545)
-	#vectorizer = CountVectorizer(stop_words='english', min_df=30) # (3867, 491), 471 w/ nltk
 	#vectorizer = TfidfVectorizer(stop_words='english', min_df=30) 
 
-	vectorizer = CountVectorizer()
+	#vectorizer = CountVectorizer(stop_words='english')
+	#vectorizer = CountVectorizer(min_df=30)
+	vectorizer = CountVectorizer(min_df=10, stop_words='english')
+	#vectorizer = CountVectorizer()
+
 	X = vectorizer.fit_transform(data['Comments'])
-	print(X.shape) # (1268, 453)
-	print(X.toarray())
-	print(vectorizer.get_feature_names())
- 
+	print(X.shape) # (1268, 88)
+	#print(X)
+	#print(X.toarray())
+	print('feature names: {}'.format(vectorizer.get_feature_names()))
+	# print(vectorizer.stop_words_)
+	#quit()
+
 	y = data['Sold']
 
-	clf = Ridge(alpha=1.0).fit(X, y)
-	print(clf)
+	#clf = Ridge(alpha=1.0).fit(X, y)
+	#print(clf)
 
-	# alphas = [0.0001, 0.001, 0.01, 0.1, 1, 10, 1e2, 1e3, 1e4, 1e5]
-	# for alpha in alphas:
-	# 	est = Ridge(alpha=alpha)
-	# 	cv_score = cross_val_score(est, X, y, cv=5)
-	# 	print('alpha: {}, cv score: {}'.format(alpha, cv_score.mean()))
+	alphas = [0.0001, 0.001, 0.01, 0.1, 1, 5, 10, 50, 1e2, 5e2, 1e3, 5e3, 1e4, 5e4, 1e5]
+	for alpha in alphas:
+		est = Ridge(alpha=alpha)
+		cv_score = cross_val_score(est, X, y, cv=5, scoring='r2')
+		print('alpha: {}, cv score: {}'.format(alpha, cv_score.mean()))
 
-	X_train, X_test, y_train, y_test = train_test_split(X, y, 
-		test_size=0.20, random_state=42)
+
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
 
 	#scaler = StandardScaler()
 	#X_train = scaler.fit_transform(X_train)
@@ -114,8 +128,8 @@ if __name__ == '__main__':
 	#for x,y in zip(X_test, y_test):
 	#	print(model.predict(x), y)
 
-	plt.scatter(y_train_pred, y_train, c = "blue", marker = "s", label = "Training data")
-	#plt.scatter(y_test_pred, y_test, c = "lightgreen", marker = "s", label = "Validation data")
+	plt.scatter(y_train_pred - y_train, y_train, c = "blue", marker = "s", label = "Training data")
+	plt.scatter(y_test_pred - y_test, y_test, c = "lightgreen", marker = "s", label = "Validation data")
 	plt.title("Linear Regression")
 	plt.xlabel("Predicted values")
 	plt.ylabel("Residuals")
