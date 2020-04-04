bda5c4b058d3c86a9ac094750d9c826c2f553f1b
statevector <cwillis@cern.ch>
2020-04-04 01:19:18 -0600

    Built pipeline to read and formatting image data for input to keras cnn model

---
+++ data_text/coin_data_cnn.py
@@ -0,0 +1,130 @@
+import numpy as np
+import pandas as pd
+import cv2
+import os
+
+import matplotlib.pyplot as plt
+import ipdb
+
+if __name__ == '__main__':
+
+	print('loading data...')
+	inputs = '/Users/cwillis/GitHub/RomanCoinData/data_text/data_scraped/'
+
+	# make a list of CSV files present in directory
+	all_files = [inputs+'/'+f for f in os.listdir(inputs) if '.csv' in f]
+	print('There are {0:d} files in dir: {1:s}'.format(len(all_files), inputs))
+
+	# load CSVs into dataframe
+	data = pd.concat([pd.read_csv(f) for f in all_files], 
+		axis=0, 
+		ignore_index=True)
+
+	data.dropna(axis=0, 
+		subset=['Image', 'Rows', 'Columns', 'Channels'], 
+		inplace=True)
+
+	data['Rows'] = data['Rows'].astype(np.int)
+	data['Columns'] = data['Columns'].astype(np.int)
+	data['Channels'] = data['Channels'].astype(np.int)
+
+	# remove non standard lots (temp)
+	data = data.loc[data['Nonstandard Lot'] == False] 
+	# weird shape at this index
+	data[data['Rows']==197]
+	
+	# isolate image with most rows to define whitespace padding
+	maxrows = data['Rows'].max()
+	print('maxrows: {}'.format(maxrows))
+
+	# check all image columns are the same size
+	assert data['Columns'].value_counts().shape[0] == 1
+
+	#halfcols = data['Columns'].max()//2
+	#print('halfcols: {}'.format(halfcols))
+
+	WHITE = [255, 255, 255]
+
+	# build feature matrices for obverse and reverse images
+	X1 = []
+	X2 = []
+	y = []
+	for index, obs in data.iterrows():
+		if index > 20: continue
+		print(index)
+		# reconstruct original image array
+		aaa = np.array(obs['Image'].split(), dtype=np.uint8) # uint8 for cv2
+		aaa = aaa.reshape(obs['Rows'], obs['Columns'], obs['Channels'])
+		print(aaa.shape)
+		# convert from PIL BGR standard to open CV RGB standard
+		img = cv2.cvtColor(aaa, cv2.COLOR_BGR2RGB)
+		rows, cols, channels = img.shape
+		print('  rows {}, cols {}, channels {}'.format(rows, cols, channels))
+		# pad the image to a consistent size to load into a dataframe
+		#imgr = img[0:130, 0:cols, 0:channels]
+		#imgr = cv2.resize(img, (130, cols), interpolation=cv2.INTER_AREA)
+		top_pad = maxrows - rows # in pixels
+		print(maxrows, rows, top_pad)
+		imgr = cv2.copyMakeBorder(img, top_pad, 0, 0, 0, 
+			cv2.BORDER_CONSTANT, 
+			value=WHITE)
+		#cv2.imshow('title', imgr)
+		#cv2.waitKey(0)
+		# grab obverse
+		#ipdb.set_trace()
+		imgr1 = imgr[:, :cols//2, :]
+		X1.append(imgr1)
+		print(imgr1.shape)
+		#cv2.imshow('title', imgr1)
+		#cv2.waitKey(0)
+		# grab reverse
+		imgr2 = imgr[:, cols//2:, :]
+		X2.append(imgr2)
+		print(imgr2.shape)
+		#cv2.imshow('title', imgr2)
+		#cv2.waitKey(0)
+		y.append(obs['Sold'])
+
+
+	# convert to numpy array and reshape for keras model
+
+	X1 = np.stack(X1, axis=0)
+	print('X1.shape: {}'.format(X1.shape))
+
+	X2 = np.stack(X2, axis=0)
+	print('X2.shape: {}'.format(X2.shape))
+
+	y = np.array(y)
+	print('y.shape: {}'.format(y.shape))
+
+
+
+	# temp assign
+	X = X1
+	X2 = None
+
+
+	# shuffle data ...
+	randomize = np.arange(X.shape[0])
+	print(randomize.shape)
+	np.random.shuffle(randomize)
+	X = X[randomize]
+	y = y[randomize]
+
+	# ... and break into training and test sets
+	n_train = int(0.80 * X.shape[0])
+	x_train, x_test = X[:n_train, :], X[n_train:, :]
+	y_train, y_test = y[:n_train], y[n_train:]
+
+	# convert to float and rescale for optimization
+	x_train = x_train.astype('float32')
+	x_test = x_test.astype('float32')
+	x_train /= 255.
+	x_test /= 255.
+	
+	print('x_train shape:', x_train.shape)
+	print('x_test shape:', x_test.shape)
+	print('y_train shape:', y_train.shape)
+	print('y_test shape:', y_test.shape)
+
+
