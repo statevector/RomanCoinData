1e15ae3fe0153dc52f65dabc05c6282809a3fa55
statevector <cwillis@cern.ch>
2020-07-27 01:48:01 -0600

    reading nero images; refined loading image process

--- data_text/coin_data_cnn.py
+++ data_text/coin_data_cnn.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import cv2
 import os
+import glob
 
 import matplotlib.pyplot as plt
 import ipdb
@@ -11,111 +12,171 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras.preprocessing.image import ImageDataGenerator
-from keras import backend
+from keras import backend as K
 
 #from keras import losses
 from keras import optimizers
 #from keras.layers.normalization import BatchNormalization
 
 def rmse(y_true, y_pred):
-	return backend.sqrt(backend.mean(backend.square(y_pred - y_true), axis=-1))
+	return K.sqrt(K.mean(K.square(y_pred - y_true), axis=-1))
+
+def r2(y_true, y_pred):
+	SS_res = K.sum(K.square(y_true - y_pred))
+	SS_tot = K.sum(K.square(y_true - K.mean(y_true)))
+	return 1 - SS_res/(SS_tot + K.epsilon())
 
 if __name__ == '__main__':
 
-	print('loading data...')
-	inputs = '/Users/cwillis/GitHub/RomanCoinData/data_text/data_scraped/'
 
-	# make a list of CSV files present in directory
-	all_files = [inputs+'/'+f for f in os.listdir(inputs) if '.csv' in f]
-	print('There are {0:d} files in dir: {1:s}'.format(len(all_files), inputs))
+	files = glob.glob("/Users/cwillis/GitHub/RomanCoinData/data_text/data_scraped/Nero*/*prepared.csv")
+	print(files)
+	data = pd.concat((pd.read_csv(f) for f in files), axis=0, sort=False, ignore_index=True) 
+	#data = data[['Image Path', 'Sold']]
+	#data.info()
+	paths = data['Image Path'].values
 
-	# load CSVs into dataframe
-	data = pd.concat([pd.read_csv(f) for f in all_files], 
-		axis=0, 
-		ignore_index=True)
-
-	data.dropna(axis=0, 
-		subset=['Image', 'Rows', 'Columns', 'Channels'], 
-		inplace=True)
-
-	data['Rows'] = data['Rows'].astype(np.int)
-	data['Columns'] = data['Columns'].astype(np.int)
-	data['Channels'] = data['Channels'].astype(np.int)
-
-	# remove non standard lots (temp)
-	data = data.loc[data['Nonstandard Lot'] == False] 
-	# weird shape at this index
-	data[data['Rows']==197]
 	
-	# isolate image with most rows to define whitespace padding
-	maxrows = data['Rows'].max()
-	print('maxrows: {}'.format(maxrows))
-
-	# check all image columns are the same size
-	assert data['Columns'].value_counts().shape[0] == 1
-
-	#halfcols = data['Columns'].max()//2
-	#print('halfcols: {}'.format(halfcols))
-
-	WHITE = [255, 255, 255]
-
-	# build feature matrices for obverse and reverse images
-	X1 = []
-	X2 = []
+	X = []
 	y = []
-	for index, obs in data.iterrows():
+	for idx, path in enumerate(paths):
+
 		#if index > 30: continue
-		print('transform image: {}'.format(index))
-		# reconstruct original image array
-		aaa = np.array(obs['Image'].split(), dtype=np.uint8) # uint8 for cv2
-		aaa = aaa.reshape(obs['Rows'], obs['Columns'], obs['Channels'])
-		print(aaa.shape)
-		# convert from PIL BGR standard to open CV RGB standard
-		img = cv2.cvtColor(aaa, cv2.COLOR_BGR2RGB)
+		print(path)
+
+		# load image
+		img = cv2.imread(path)
 		rows, cols, channels = img.shape
-		print('rows {}, cols {}, channels {}'.format(rows, cols, channels))
-		# pad the image to a consistent size to load into a dataframe
-		#imgr = img[0:130, 0:cols, 0:channels]
-		#imgr = cv2.resize(img, (130, cols), interpolation=cv2.INTER_AREA)
-		top_pad = maxrows - rows # in pixels
-		print(maxrows, rows, top_pad)
-		imgr = cv2.copyMakeBorder(img, top_pad, 0, 0, 0, 
-			cv2.BORDER_CONSTANT, 
-			value=WHITE)
-		#cv2.imshow('title', imgr)
+		print('original   ', img.shape)
+
+		# convert to gray scale
+		img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+		print('gray       ', img.shape)
+		#cv2.imshow('gray', img)
 		#cv2.waitKey(0)
+
+		# resize image to consistent row-shape
+		maxrows = 128
+		scale_percent = float(maxrows/rows) # percent of original size
+		#print(scale_percent)
+		width = int(img.shape[1] * scale_percent)
+		height = int(img.shape[0] * scale_percent)
+		dim = (width, height)
+
+		# resize image
+		img = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+		print('resized    ', img.shape)
+		#cv2.imshow('resized', img)
+		#cv2.waitKey(0)
+
 		# grab obverse
-		#ipdb.set_trace()
-		imgr1 = imgr[:, :cols//2, :]
-		X1.append(imgr1)
-		print(imgr1.shape)
-		#cv2.imshow('title', imgr1)
+		img1 = img[:, :img.shape[1]//2]
+		print('obverse    ', img1.shape)
+		#cv2.imshow('obverse', img1)
 		#cv2.waitKey(0)
-		# grab reverse
-		imgr2 = imgr[:, cols//2:, :]
-		X2.append(imgr2)
-		print(imgr2.shape)
-		#cv2.imshow('title', imgr2)
+
+		# resize obverse image
+		img1 = cv2.resize(img1, (128, 128), interpolation=cv2.INTER_AREA)
+		print('resized    ', img1.shape)
+		#cv2.imshow('resized', img1)
 		#cv2.waitKey(0)
-		y.append(obs['Sold'])
-		print('-------------------------')
 
-	# convert to numpy array and reshape for keras model
+		X.append(img1)
+		y.append(data['Sold'].iloc[idx])
 
-	X1 = np.stack(X1, axis=0)
-	print('X1.shape: {}'.format(X1.shape))
 
-	X2 = np.stack(X2, axis=0)
-	print('X2.shape: {}'.format(X2.shape))
+	X = np.stack(X, axis=0)
+	print('X.shape: {}'.format(X.shape))
 
 	y = np.array(y)
 	print('y.shape: {}'.format(y.shape))
 
 
 
-	# temp assign
-	X = X1
-	X2 = None
+
+	# # isolate image with greatest number of 
+	# # rows to define whitespace padding
+	# maxrows = 128
+	# print('maxrows: {}'.format(maxrows))
+
+	# #halfcols = data['Columns'].max()//2
+	# #print('halfcols: {}'.format(halfcols))
+
+	# resize = False
+
+	# build feature matrices for obverse and reverse images
+	# X1 = []
+	# X2 = []
+	# y = []
+	
+	# for idx, path in enumerate(paths):
+
+	# 	# crop image top in pixels
+	# 	if(rows>maxrows):
+	# 		#img = img[0:maxrows, :, :]
+	# 		img = img[0:maxrows, :]
+	# 	print('cropped    ', img.shape)
+
+	# 	# pad image top in pixels
+	# 	if(rows<maxrows):
+	# 		top_pad = maxrows - rows
+	# 		#print(maxrows, rows, top_pad)
+	# 		WHITE = [255, 255, 255]
+	# 		img = cv2.copyMakeBorder(img, top_pad, 0, 0, 0,  cv2.BORDER_CONSTANT, value=WHITE)
+	# 	print('padded     ', img.shape)
+	# 	#cv2.imshow('title', img)
+	# 	#cv2.waitKey(0)
+
+	# 	#ipdb.set_trace()
+
+	# 	# grab obverse
+	# 	#######img1 = img[:, :cols//2, :]
+	# 	img1 = img[:, :cols//2]
+	# 	print('obverse    ', img1.shape)
+	# 	#cv2.imshow('title', imgr1)
+	# 	#cv2.waitKey(0)
+
+	# 	# resize obverse image
+	# 	if(resize):
+	# 		img1 = cv2.resize(img1, (32, 32), interpolation=cv2.INTER_AREA)
+	# 		print('obv. R     ', img1.shape)
+	# 		#cv2.imshow('title', img1)
+	# 		#cv2.waitKey(0)
+
+	# 	# grab reverse
+	# 	#######img2 = img[:, cols//2:, :]
+	# 	img2 = img[:, cols//2:]
+	# 	print('reverse    ', img2.shape)
+	# 	#cv2.imshow('title', img2)
+	# 	#cv2.waitKey(0)
+
+	# 	# resize reverse image
+	# 	if(resize):
+	# 		img2 = cv2.resize(img2, (32, 32), interpolation=cv2.INTER_AREA)
+	# 		print('rev. R     ', img2.shape)
+	# 		cv2.imshow('title', img2)
+	# 		cv2.waitKey(0)
+
+	# 	X2.append(img2)
+	# 	X1.append(img1)
+	# 	y.append(data['Sold'].iloc[idx])
+		
+	# 	print('-------------------------')
+
+	# convert to numpy array and reshape for keras model
+
+	# X1 = np.stack(X1, axis=0)
+	# print('X1.shape: {}'.format(X1.shape))
+
+	# X2 = np.stack(X2, axis=0)
+	# print('X2.shape: {}'.format(X2.shape))
+
+	# y = np.array(y)
+	# print('y.shape: {}'.format(y.shape))
+
+	# # temp assign
+	# X = X1
+	# X2 = None
 
 
 	# shuffle data ...
@@ -136,6 +197,11 @@ if __name__ == '__main__':
 
 	X_test = X_test.astype('float32')
 	X_test /= 255.
+
+	# NORMALIZE Y
+	y_train = np.log(y_train)
+	y_test = np.log(y_test)
+
 	
 	print('x_train shape:', X_train.shape)
 	print('x_test shape:', X_test.shape)
@@ -159,85 +225,95 @@ if __name__ == '__main__':
 	    #rescale=1./255)
 	    )
 
+	# make rank 4 tensors for datagen
+	X_train = X_train.reshape((*X_train.shape,1))
+	X_test = X_test.reshape((*X_test.shape,1))
+
 	# compute quantities required for featurewise normalization
 	train_datagen = datagen.fit(X_train)
 
 	# plot a 3x3 grid of normalized images (sanity check)
-	for i, (X_batch, y_batch) in enumerate(datagen.flow(X_train, y_train, batch_size=9)):
-		#fig = plt.figure()
-		print(i, X_batch.shape)
-		for j, x in enumerate(X_batch):
-			# subplot(nrows, ncols, index, **kwargs)
-			plt.subplot(3, 3, j+1)
-			plt.imshow(x)
-		plt.show()
-		if i>=4: 
-			plt.close('all')
-			break
+	test_generator = False
+	if(test_generator):
+		for i, (X_batch, y_batch) in enumerate(datagen.flow(X_train, y_train, batch_size=9)):
+			print('i=', i, X_batch.shape)
+			for j, x in enumerate(X_batch):
+				print('j=', j, x.shape)
+				# subplot(nrows, ncols, index, **kwargs)
+				plt.subplot(3, 3, j+1)
+				x = x.reshape(128, 128)
+				plt.imshow(x, cmap='gray', vmin=0, vmax=1)
+			plt.show()
+			if i>=4: 
+				plt.close('all')
+				break
 
 
 
 
 
 	# flatten inputs for baseline tests
-	X_train_b = X_train.reshape(len(X_train),-1)
-	X_test_b = X_test.reshape(len(X_test),-1)
+	# X_train_b = X_train.reshape(len(X_train),-1)
+	# X_test_b = X_test.reshape(len(X_test),-1)
 
-	# baseline model
-	model = Sequential()
-	model.add(Dense(32, activation='relu', input_shape = X_train_b.shape[1:]))
-	model.add(Dropout(rate=0.5))
-	model.add(Dense(16, activation='relu'))
-	model.add(Dropout(rate=0.5))
-	model.add(Dense(8, activation='relu'))
-	model.add(Dropout(rate=0.5))
-	model.add(Dense(4, activation='relu'))
-	model.add(Dropout(rate=0.5))
-	model.add(Dense(1)) # possible to add regularization
-	sgd = optimizers.SGD(learning_rate=0.01, momentum=0.0, nesterov=False)
-
-	model.compile(loss='mse', optimizer=sgd, metrics=['mae'])
-	print(model.summary())
-	history = model.fit(X_train_b, y_train, batch_size=128, epochs=1000, verbose=1, validation_data=(X_test_b, y_test))
-	# score = model.evaluate(X_test, y_test, verbose=0)
+	# # baseline model
+	# model = Sequential()
+	# model.add(Dense(64, activation='relu', input_shape = X_train_b.shape[1:]))
+	# #model.add(Dropout(rate=0.5))
+	# model.add(Dense(32, activation='relu'))
+	# #model.add(Dropout(rate=0.5))
+	# model.add(Dense(16, activation='relu'))
+	# #model.add(Dropout(rate=0.5))
+	# model.add(Dense(8, activation='relu'))
+	# #model.add(Dropout(rate=0.5))
+	# model.add(Dense(4, activation='relu'))
+	# model.add(Dense(1)) # possible to add regularization
+	# sgd = optimizers.SGD(learning_rate=0.001, momentum=0.0, nesterov=False)
+	# model.compile(loss='mae', optimizer=sgd, metrics=[r2, 'mae'])
+	# print(model.summary())
+	# history = model.fit(X_train_b, y_train, batch_size=128, epochs=1000, verbose=1, 
+	# 	validation_data=(X_test_b, y_test))
+	# score = model.evaluate(X_test_b, y_test, verbose=0)
 	# print('Test loss:', score[0])
 	# print('Test accuracy:', score[1])
 
-
 	# CNN model
-	# model = Sequential()
-	# model.add(Conv2D(16, kernel_size=(5, 5), activation='relu', padding='same', 
-	# 	input_shape=X.shape[1:]))
-	# model.add(MaxPooling2D(pool_size=(2, 2)))
-	# model.add(Conv2D(16, kernel_size=(3, 3), activation='relu', padding='same'))
-	# model.add(MaxPooling2D(pool_size=(2, 2)))
-	# model.add(Conv2D(16, kernel_size=(3, 3), activation='relu', padding='same'))
-	# model.add(MaxPooling2D(pool_size=(2, 2)))
-	# #model.add(Conv2D(16, kernel_size=(5, 5), activation='relu', padding='same'))
-	# #model.add(MaxPooling2D(pool_size=(2, 2)))
-	# #model.add(BatchNormalization())
-	# model.add(Flatten())
-	# model.add(Dense(16, activation='relu'))
-	# model.add(Dropout(rate=0.5))
-	# model.add(Dense(1)) # possible to add regularization
+	model = Sequential()
+	model.add(Conv2D(16, kernel_size=(3, 3), activation='relu', padding='same', 
+	 	input_shape=X_train.shape[1:])) # reduced from 16 and improved
+	model.add(MaxPooling2D(pool_size=(3, 3)))
+	model.add(Conv2D(8, kernel_size=(3, 3), activation='relu', padding='same'))
+	model.add(MaxPooling2D(pool_size=(3, 3)))  # reduced from 16 and improved
+	#model.add(Conv2D(4, kernel_size=(3, 3), activation='relu', padding='same'))
+	#model.add(MaxPooling2D(pool_size=(3, 3)))
+	model.add(Flatten())
+	model.add(Dense(64, activation='relu'))
+	#model.add(Dense(32, activation='relu'))
+	#model.add(Dense(16, activation='relu'))
+	#model.add(Dense(8, activation='relu'))
+	#model.add(Dense(4, activation='relu'))
+	model.add(Dense(1)) # possible to add regularization
+	sgd = optimizers.SGD(learning_rate=0.0001, momentum=0.0, nesterov=False)
+	adam = optimizers.Adam(learning_rate=0.001)
+	model.compile(loss=rmse, optimizer=adam, metrics=[r2, rmse])
+	print(model.summary())
 
-	# model.compile(loss='mse', optimizer='adam', metrics=['mae'])
-	#print(model.summary())
+	history = model.fit(X_train, y_train, batch_size=256, epochs=100, verbose=1, 
+		validation_data=(X_test, y_test))
 
 	# # keras run parameters
 	# batch_size = 64 # 1/100 the data
 	# epochs = 10 # 31... seemed good
 
-	# history = model.fit_generator(
-	# 	datagen.flow(X_train, y_train, shuffle=True, batch_size=batch_size),
-	# 	validation_data=(X_test, y_test),
-	# 	steps_per_epoch=X_train.shape[0]//batch_size, 
-	# 	epochs=epochs, 
-	# 	use_multiprocessing=True, 
-	# 	verbose=1)
+	history = model.fit_generator(
+		datagen.flow(X_train, y_train, shuffle=True, batch_size=256),
+		validation_data=(X_test, y_test),
+		steps_per_epoch=X_train.shape[0]//256, 
+		epochs=100, 
+		use_multiprocessing=True, 
+		verbose=1)
 
 	# score = model.evaluate(X_test, y_test, verbose=0)
-
 	# print('Test loss:', score[0])
 	# print('Test accuracy:', score[1])
 
