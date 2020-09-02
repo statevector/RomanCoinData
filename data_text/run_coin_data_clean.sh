0e3d6146662aa8c5f7194d593d86ac9e84cc1633
statevector <cwillis@cern.ch>
2020-09-02 01:06:02 -0600

    updated cleaning run script for new data

--- data_text/run_coin_data_clean.sh
+++ data_text/run_coin_data_clean.sh
@@ -2,33 +2,55 @@
 
 # Augustus
 
-python3 coin_data_clean.py data_scraped/Augustus_Aur_EA1/Augustus_Aur_EA1.csv
-python3 coin_data_clean.py data_scraped/Augustus_Aur_PA1/Augustus_Aur_PA1.csv
+# python3 coin_data_clean.py data_scraped/Augustus_Aur_EA1/Augustus_Aur_EA1.csv
+# python3 coin_data_clean.py data_scraped/Augustus_Aur_PA1/Augustus_Aur_PA1.csv
 
-python3 coin_data_clean.py data_scraped/Augustus_Cis_EA1/Augustus_Cis_EA1.csv
-python3 coin_data_clean.py data_scraped/Augustus_Cis_PA1/Augustus_Cis_PA1.csv
+# python3 coin_data_clean.py data_scraped/Augustus_Cis_EA1/Augustus_Cis_EA1.csv
+# python3 coin_data_clean.py data_scraped/Augustus_Cis_PA1/Augustus_Cis_PA1.csv
 
-python3 coin_data_clean.py data_scraped/Augustus_Den_EA1/Augustus_Den_EA1.csv
-python3 coin_data_clean.py data_scraped/Augustus_Den_EA2/Augustus_Den_EA2.csv
-python3 coin_data_clean.py data_scraped/Augustus_Den_EA3/Augustus_Den_EA3.csv
-python3 coin_data_clean.py data_scraped/Augustus_Den_EA4/Augustus_Den_EA4.csv
+# python3 coin_data_clean.py data_scraped/Augustus_Den_EA1/Augustus_Den_EA1.csv
+# python3 coin_data_clean.py data_scraped/Augustus_Den_EA2/Augustus_Den_EA2.csv
+# python3 coin_data_clean.py data_scraped/Augustus_Den_EA3/Augustus_Den_EA3.csv
+# python3 coin_data_clean.py data_scraped/Augustus_Den_EA4/Augustus_Den_EA4.csv
 
-python3 coin_data_clean.py data_scraped/Augustus_Den_PA1/Augustus_Den_PA1.csv
-python3 coin_data_clean.py data_scraped/Augustus_Den_PA2/Augustus_Den_PA2.csv
-python3 coin_data_clean.py data_scraped/Augustus_Den_PA3/Augustus_Den_PA3.csv
+# python3 coin_data_clean.py data_scraped/Augustus_Den_PA1/Augustus_Den_PA1.csv
+# python3 coin_data_clean.py data_scraped/Augustus_Den_PA2/Augustus_Den_PA2.csv
+# python3 coin_data_clean.py data_scraped/Augustus_Den_PA3/Augustus_Den_PA3.csv
 
-python3 coin_data_clean.py data_scraped/Augustus_Ses_EA1/Augustus_Ses_EA1.csv
-python3 coin_data_clean.py data_scraped/Augustus_Ses_PA1/Augustus_Ses_PA1.csv
+# python3 coin_data_clean.py data_scraped/Augustus_Ses_EA1/Augustus_Ses_EA1.csv
+# python3 coin_data_clean.py data_scraped/Augustus_Ses_PA1/Augustus_Ses_PA1.csv
 
 # Nero
 
-python3 coin_data_clean.py data_scraped/Nero_Aur_EA1/Nero_Aur_EA1.csv
-python3 coin_data_clean.py data_scraped/Nero_Aur_PA1/Nero_Aur_PA1.csv
+# python3 coin_data_clean.py data_scraped/Nero_Aur_EA1/Nero_Aur_EA1.csv
+# python3 coin_data_clean.py data_scraped/Nero_Aur_PA1/Nero_Aur_PA1.csv
 
-python3 coin_data_clean.py data_scraped/Nero_Den_EA1/Nero_Den_EA1.csv
-python3 coin_data_clean.py data_scraped/Nero_Den_EA2/Nero_Den_EA2.csv
-python3 coin_data_clean.py data_scraped/Nero_Den_PA1/Nero_Den_PA1.csv
+# python3 coin_data_clean.py data_scraped/Nero_Den_EA1/Nero_Den_EA1.csv
+# python3 coin_data_clean.py data_scraped/Nero_Den_EA2/Nero_Den_EA2.csv
+# python3 coin_data_clean.py data_scraped/Nero_Den_PA1/Nero_Den_PA1.csv
 
-python3 coin_data_clean.py data_scraped/Nero_Ses_EA1/Nero_Ses_EA1.csv
-python3 coin_data_clean.py data_scraped/Nero_Ses_EA2/Nero_Ses_EA2.csv
-python3 coin_data_clean.py data_scraped/Nero_Ses_PA1/Nero_Ses_PA1.csv
+# python3 coin_data_clean.py data_scraped/Nero_Ses_EA1/Nero_Ses_EA1.csv
+# python3 coin_data_clean.py data_scraped/Nero_Ses_EA2/Nero_Ses_EA2.csv
+# python3 coin_data_clean.py data_scraped/Nero_Ses_PA1/Nero_Ses_PA1.csv
+
+# AP
+
+python3 coin_data_clean.py data_scraped/Pius_Aur_EA1/Pius_Aur_EA1.csv
+python3 coin_data_clean.py data_scraped/Pius_Aur_PA1/Pius_Aur_PA1.csv
+
+python3 coin_data_clean.py data_scraped/Pius_Den_EA1/Pius_Den_EA1.csv
+python3 coin_data_clean.py data_scraped/Pius_Den_EA2/Pius_Den_EA2.csv
+python3 coin_data_clean.py data_scraped/Pius_Den_EA3/Pius_Den_EA3.csv
+python3 coin_data_clean.py data_scraped/Pius_Den_PA1/Pius_Den_PA1.csv
+
+python3 coin_data_clean.py data_scraped/Pius_Ses_EA1/Pius_Ses_EA1.csv
+python3 coin_data_clean.py data_scraped/Pius_Ses_EA2/Pius_Ses_EA2.csv
+python3 coin_data_clean.py data_scraped/Pius_Ses_EA3/Pius_Ses_EA3.csv
+python3 coin_data_clean.py data_scraped/Pius_Ses_PA1/Pius_Ses_PA1.csv
+
+
+
+
+
+
+
